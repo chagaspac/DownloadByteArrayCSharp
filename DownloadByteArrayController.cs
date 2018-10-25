a83a10d124b5ee1f483da76adaf668a6bd1aeeda
Pedro Chagas <chagaspac@gmail.com>
2018-10-25 12:10:29 -0300

    add file

---
+++ DownloadByteArrayController.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web.Http;
+
+namespace libpdf.Controllers
+{
+    public class DownloadByteArrayController : ApiController
+    {
+        // GET api/values/5
+        public HttpResponseMessage Get(int id)
+        {
+            byte[] a = new byte[0];
+
+            HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.OK);
+            response.Content = new ByteArrayContent(a);
+            response.Content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment");
+            response.Content.Headers.ContentDisposition.FileName = "anynamehere.pdf";
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/pdf");
+
+            return response;
+        }
+    }
+}
